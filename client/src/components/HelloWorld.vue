98dd44a0817cfa790da31c79c0050a41720fd8ed
2518106470@qq.com <2518106470@qq.com>
2024-06-30 18:35:08 +0800

    第二天上课所学的，关于router

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{data.name}}</p>
+</template>
+
+<script>
+import { reactive } from "vue"
+export default{
+	name:"HelloWorld",
+	setup() {
+		const data = reactive({
+			name:"CWJ",
+			age:18
+		})
+		return{
+			data
+		}
+	}
+}
+</script>
